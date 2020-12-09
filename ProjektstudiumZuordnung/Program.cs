1d12e24851e47d756961e10bb4ea33c26f873498
Nick Häcker <nick.haecker@auth.freshx.de>
2020-12-09 13:21:30 +0100

    init new folder

---
+++ ProjektstudiumZuordnung/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjektstudiumZuordnung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
