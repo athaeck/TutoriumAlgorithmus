809381115c066b92df83fca0bd0b683abcc88245
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2020-12-18 19:44:20 +0100

    wichtig! durchlesen!
    in der MEthode ,,InitStudents", ,,InitInitiators" und ,,InitProjects" ist jeweils ein switch case in dem jetzt die Objekte wie bei den Pokemon einzeln instanziert werden müssen, der string ,,two" bezieht sich auf den zweiten Probefall für den ihr Daten erstellt habt, also das ist der zweite Screenshot in der Gruppe, ,,three" bezieht sich dann auf den den dritten, den ersten also den default wert wenn für ,,test_number" nicht drin steht ist der erste den ich machen werde

--- ProjektstudiumZuordnung/Program.cs
+++ ProjektstudiumZuordnung/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
 namespace ProjektstudiumZuordnung
 {
     class Program
@@ -7,14 +9,64 @@ namespace ProjektstudiumZuordnung
         public static List<Initiator> initiatorList { get; set; }
         public static List<Project> projectList { get; set; }
         public static List<Student> studentList { get; set; }
+
+        private static string test_number = "";
         static void Main(string[] args)
         {
-            // AStudent a = new Student()
+            Console.WriteLine("Begin Initiation");
             GetData();
+            Console.WriteLine("Loaded Data");
         }
         static void GetData()
         {
-
+            InitStudents();
+            InitInitiators();
+            InitProjects();
+        }
+        static void InitStudents()
+        {
+            switch (test_number)
+            {
+                case "two":
+                    Console.WriteLine("TBD");
+                    break;
+                case "three":
+                    Console.WriteLine("TBD");
+                    break;
+                default:
+                    Console.WriteLine("Go");
+                    break;
+            }
+        }
+        static void InitInitiators()
+        {
+            switch (test_number)
+            {
+                case "two":
+                    Console.WriteLine("TBD");
+                    break;
+                case "three":
+                    Console.WriteLine("TBD");
+                    break;
+                default:
+                    Console.WriteLine("TBD");
+                    break;
+            }
+        }
+        static void InitProjects()
+        {
+            switch (test_number)
+            {
+                case "two":
+                    Console.WriteLine("TBD");
+                    break;
+                case "three":
+                    Console.WriteLine("TBD");
+                    break;
+                default:
+                    Console.WriteLine("TBD");
+                    break;
+            }
         }
     }
 }
